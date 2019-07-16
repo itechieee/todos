f2ba00484709870d6f7bbbab39c57ea83f26fc8b
Prabu <prabu.k@contentcreation.com.my>
2019-07-16 19:23:12 +0800

    Add Todo form with simple JS

--- src/components/Todo/TodoForm/todoFormComponent.js
+++ src/components/Todo/TodoForm/todoFormComponent.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 function TodoForm(props) {
 
-  function handleSubmit() {
-    console.log('form submittt');
+  function handleSubmit(e) {
+    e.preventDefault();    
+    if(document.getElementById('todo').value === '') {
+        document.getElementById('todoError').value = 'Should not be empty';
+    } else {
+        console.log(document.getElementById('todo').value);
+        document.getElementById('todo').value = '';
+        document.getElementById('todoError').innerText = '';
+    }
   }
   return (
       <form onSubmit={handleSubmit}>
-        <label> Name: <input type="text" name="name" /> </label>
+        <label> Name: <input id="todo" type="text" name="name" /> </label>
         <input type="submit" value="Submit" />
+        <span id="todoError"></span>
       </form>
   );
 }
